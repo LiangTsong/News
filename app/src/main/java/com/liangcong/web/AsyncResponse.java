58e7c99ace74cfb604bb6e3e4cfe32a20cfc3d4e
liangcong <liang-cong@foxmail.com>
2018-09-06 18:38:47 +0800

    1. Multi-tabs 2. Loading animation 3. Display news by itself, no need to rotate the screen
    NEXT TO DO:
    Load each tab only when users scroll to it.

--- app/src/main/java/com/liangcong/web/AsyncResponse.java
+++
@@ -1,9 +0,0 @@
-package com.liangcong.web;
-
-
-import java.util.List;
-
-public interface AsyncResponse {
-    public void onDataReceivedSuccess(List<TencentNewsXmlParser.NewsItem> listData);
-    public void onDataReceivedFailed();
-}
