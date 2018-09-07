6e44e2b1e6e8af96885450523a0e486e3b796daf
liangcong <liang-cong@foxmail.com>
2018-09-07 15:10:47 +0800

    Cahce.

--- News/app/src/main/java/com/liangcong/news/DisplayNewsActivity.java
+++ News/app/src/main/java/com/liangcong/news/DisplayNewsActivity.java
@@ -1,10 +1,15 @@
 package com.liangcong.news;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.os.Build;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
@@ -19,16 +24,16 @@ public class DisplayNewsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_news);
 
+        ActionBar ab = getSupportActionBar();
+        ab.setDisplayHomeAsUpEnabled(true);
+
         // Get the Intent that started this activity and extract the string
         Intent intent = getIntent();
         String url = intent.getStringExtra("NEWS_URL");
 
         WebView webView = (WebView) findViewById(R.id.web_view);
 
-        WebSettings webSettings = webView.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setDomStorageEnabled(true);
-
+        webView.getSettings().setJavaScriptEnabled(true);
 
         webView.setWebChromeClient(new WebChromeClient() {
 
@@ -39,6 +44,26 @@ public class DisplayNewsActivity extends AppCompatActivity {
                 actionbar.setTitle(title);
             }
         });
+
+        webView.getSettings().setRenderPriority(WebSettings.RenderPriority.HIGH);
+        // 建议缓存策略为，判断是否有网络，有的话，使用LOAD_DEFAULT,无网络时，使用LOAD_CACHE_ELSE_NETWORK
+
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        // 开启DOM storage API 功能
+        webView.getSettings().setDomStorageEnabled(true);
+        // 开启database storage API功能
+        webView.getSettings().setDatabaseEnabled(true);
+        // String cacheDirPath = getFilesDir().getAbsolutePath()
+        //         + APP_CACHE_DIRNAME
+       /* String cacheDirPath = ConfigUtil.getCacheDir();
+                + APP_CACHE_DIRNAME;
+        Log.i("CACHE", "cachePath=" + cacheDirPath);
+        // 设置数据库缓存路径
+        webView.getSettings().setDatabasePath(cacheDirPath);
+        webView.getSettings().setAppCachePath(cacheDirPath);*/
+        // 开启Application Cache功能
+        webView.getSettings().setAppCacheEnabled(true);
+
         webView.loadUrl(url);
 
         //app内打开
@@ -50,5 +75,18 @@ public class DisplayNewsActivity extends AppCompatActivity {
             }
         });
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            webView.evaluateJavascript(
+                    "(function() {return ('<html>'+document.getElementsByTagName('html')[0].innerHTML+'</html>');})();",
+                    new ValueCallback<String>() {
+                        @Override
+                        public void onReceiveValue(String html) {
+                            Log.d("HTML", "whd >>html:" + html);
+                        }
+                    });
+        }
+
+
     }
 }
+
