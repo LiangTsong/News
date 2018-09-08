34e10ac79ab7d4dbe77b7028d01d7535394bca94
liangcong <liang-cong@foxmail.com>
2018-09-09 04:48:35 +0800

    Rename package.

--- News/app/src/main/java/com/liangcong/web/AsyncResponse.java
+++
@@ -1,9 +0,0 @@
-package com.liangcong.web;
-
-
-import java.util.ArrayList;
-
-public interface AsyncResponse {
-    public void onDataReceivedSuccess(ArrayList<TencentNewsXmlParser.NewsItem> listData);
-    public void onDataReceivedFailed();
-}
